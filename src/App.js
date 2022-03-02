01d3831c351fd2a4a20f0f4bbdf3640480adf1bd
DESKTOP-NPTOF76\Gökalp AVCU <gokalp.avcu@santsg.com>
2022-03-02 13:39:33 +0300

    add js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import Header from './components/Header';
 
 function App() {
+
+  const [money, setMoney] = useState(100) // baslangic parasi olarak 100$ belirledik bu {money}'de ki para
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <>
+    <Header money={money}/>
+    </>
+
+   
+   
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header({money}) { //prop olarak money aldık
+  return (
+      
+    <>
+    <div>
+
+        Harcamak için {money} $ paranız var.
+
+    </div>
+
+    
+  </>
+      
+
+   
+  )
+}
+
+export default Header
